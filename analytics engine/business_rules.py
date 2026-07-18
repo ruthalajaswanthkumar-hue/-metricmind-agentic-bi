aa03b6a0ff2294db4e7ed79cac6f2d2a905a3470
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-18 18:10:14 +0530

    Update business_rules.py

--- analytics engine/business_rules.py
+++ analytics engine/business_rules.py
@@ -1,1 +1,14 @@
 
+"""
+Business Rules
+"""
+
+class BusinessRules:
+
+    def calculate_profit(self, revenue, cost):
+        return revenue - cost
+
+    def calculate_margin(self, profit, revenue):
+        if revenue == 0:
+            return 0
+        return (profit / revenue) * 100
