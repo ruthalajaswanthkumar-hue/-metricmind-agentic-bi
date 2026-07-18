fcf8db1ce5fee7fe56111afd32453408758344a0
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-18 18:11:09 +0530

    Update response_formatter.py

--- analytics engine/response_formatter.py
+++ analytics engine/response_formatter.py
@@ -1,1 +1,14 @@
 
+"""
+Response Formatter
+"""
+
+class ResponseFormatter:
+
+    def format(self, title, chart, data):
+
+        return {
+            "title": title,
+            "chart": chart,
+            "data": data
+        }
