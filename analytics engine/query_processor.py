e4d6624ca265621926f979ff4b301cd68bbebe03
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-18 18:06:25 +0530

    Update query_processor.py

--- analytics engine/query_processor.py
+++ analytics engine/query_processor.py
@@ -1,1 +1,18 @@
 
+"""
+Query Processor
+
+Extracts important information from user questions.
+"""
+
+class QueryProcessor:
+
+    def extract(self, query):
+
+        return {
+            "query": query,
+            "metric": None,
+            "dimension": None,
+            "time": None,
+            "status": "Processing"
+        }
