ae28c74103bc55ea68a210c5f0269774e084c032
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-20 11:54:12 +0530

    Updated day5 insight_generator.py

--- analytics engine/insight_generator.py
+++ analytics engine/insight_generator.py
@@ -1,1 +1,16 @@
 
+"""
+Business Insight Generator
+"""
+
+sample_insights = [
+
+    "Revenue increased compared to last month.",
+
+    "South Region generated the highest sales.",
+
+    "Electronics is the best performing category.",
+
+    "Customer growth is improving steadily."
+
+]
