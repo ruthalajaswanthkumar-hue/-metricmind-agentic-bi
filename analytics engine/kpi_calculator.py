f6db955061ac523d0b41ae76ab79994faf476b85
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-20 11:49:34 +0530

    Updated day 5 kpi_calculator.py

--- analytics engine/kpi_calculator.py
+++ analytics engine/kpi_calculator.py
@@ -1,1 +1,18 @@
 
+"""
+MetricMind Analytics Engine
+
+KPI Calculator
+"""
+
+def calculate_revenue(data):
+    return "Calculates total revenue"
+
+def calculate_profit(data):
+    return "Calculates total profit"
+
+def calculate_orders(data):
+    return "Calculates total orders"
+
+def calculate_customers(data):
+    return "Calculates total customers"
