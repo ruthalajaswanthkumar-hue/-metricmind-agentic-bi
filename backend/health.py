a3096da63d86c2c3f02af9343b3e1f5a90f67d88
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-21 00:39:42 +0530

    Updated health.py

--- backend/health.py
+++ backend/health.py
@@ -1,1 +1,19 @@
 
+ from fastapi import APIRouter
+
+# Create a router
+router = APIRouter()
+
+# Home API
+@router.get("/")
+def root():
+    return {
+        "message": "MetricMind Backend Running"
+    }
+
+# Health Check API
+@router.get("/health")
+def health():
+    return {
+        "status": "Running"
+    }
