fa1b008484eed591dbb7f6c6a9779c537f1e25a5
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-20 22:55:40 +0530

    Update chat.py

--- backend/api/chat.py
+++ backend/api/chat.py
@@ -1,23 +1,12 @@
  from fastapi import APIRouter
-from pydantic import BaseModel
+
+from backend.schemas.chat_request import ChatRequest
+from backend.schemas.chat_response import ChatResponse
 
 router = APIRouter()
 
-# Request Model
-class ChatRequest(BaseModel):
-    question: str
-
-# Response Model
-class ChatResponse(BaseModel):
-    response: str
-
 @router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
-
-    if request.question.strip() == "":
-        return {
-            "response": "Question cannot be empty."
-        }
 
     return {
         "response": "Processing..."
