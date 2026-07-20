89e423a7413608074889423302b7451842921a8e
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-20 11:53:00 +0530

    Updated day5 analytics_config.py

--- analytics engine/analytics_config.py
+++ analytics engine/analytics_config.py
@@ -1,1 +1,22 @@
 
+"""
+Analytics Engine Configuration
+"""
+
+DEFAULT_CURRENCY = "INR"
+
+DEFAULT_TIME_PERIOD = "Monthly"
+
+SUPPORTED_CHARTS = [
+
+    "Bar",
+
+    "Line",
+
+    "Pie",
+
+    "Area",
+
+    "KPI Card"
+
+]
