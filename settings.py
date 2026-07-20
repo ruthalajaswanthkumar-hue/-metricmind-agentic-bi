bc4fb300e518102a9e0221f8d1fadca2f3ea124e
vanshika <vanshika.24SCSE1420117@galgotiasuniversity.ac.in>
2026-07-20 17:35:10 +0530

    day 1 ,day 2,day 3 work completed
    all the files for backend has  been intialize and completed .

---
+++ settings.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# Load backend/.env
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_DATABASE = os.getenv("SNOWFLAKE_DATABASE")
+SNOWFLAKE_SCHEMA = os.getenv("SNOWFLAKE_SCHEMA")
+SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
+print("DATABASE_URL =", DATABASE_URL)
