26c1d9e1427a97cb03436c7efc1bff4a2b5e497a
sreeraj k <srjkk21@gmail.com>
2026-07-21 17:52:27 +0530

    Create stg_customer.sql

---
+++ semantic-layer/stg_customer.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT
+    TRIM(CustomerID) AS customer_id,
+    TRIM(CustomerName) AS customer_name,
+    TRIM(Region) AS region,
+    TRIM(City) AS city
+
+FROM {{ source('raw', 'Customers') }}
+
+WHERE CustomerID IS NOT NULL
+  AND CustomerName IS NOT NULL;
