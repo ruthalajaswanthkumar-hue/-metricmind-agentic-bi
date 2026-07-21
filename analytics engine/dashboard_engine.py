e454917482ad61aa0f2c90acf1fff16c7095dbc7
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-21 16:40:41 +0530

    Update dashboard_engine.py

--- analytics engine/dashboard_engine.py
+++ analytics engine/dashboard_engine.py
@@ -1,1 +1,23 @@
 
+"""
+Dashboard Intelligence Engine
+"""
+
+class DashboardEngine:
+
+    @staticmethod
+    def dashboard_summary():
+
+        return {
+            "cards": [
+                "Revenue",
+                "Profit",
+                "Orders",
+                "Customers"
+            ],
+            "charts": [
+                "Bar Chart",
+                "Line Chart",
+                "Pie Chart"
+            ]
+        }
