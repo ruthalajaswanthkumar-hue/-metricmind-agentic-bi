5bfbc44f76e6cb6bc88cd2e1e87daded20c90e23
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-21 16:42:55 +0530

    Updated day 8 insight_test.py

--- analytics engine/insight_test.py
+++ analytics engine/insight_test.py
@@ -1,1 +1,6 @@
 
+from insight_generator import InsightGenerator
+
+print(InsightGenerator.generate("Revenue",120000,100000))
+print(InsightGenerator.generate("Profit",25000,30000))
+print(InsightGenerator.generate("Orders",500,500))
