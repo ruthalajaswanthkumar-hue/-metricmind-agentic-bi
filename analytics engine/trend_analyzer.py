ba55276622297ee90c9958a2a97cbe624a3807eb
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-21 16:38:23 +0530

    Updated trend_analyzer.py

--- analytics engine/trend_analyzer.py
+++ analytics engine/trend_analyzer.py
@@ -1,1 +1,31 @@
 
+"""
+Trend Analyzer
+Analyzes business trends.
+"""
+
+class TrendAnalyzer:
+
+    @staticmethod
+    def analyze(current_value, previous_value):
+
+        if current_value > previous_value:
+            return {
+                "trend": "Increasing",
+                "direction": "Up",
+                "percentage": round(((current_value - previous_value) / previous_value) * 100, 2)
+            }
+
+        elif current_value < previous_value:
+            return {
+                "trend": "Decreasing",
+                "direction": "Down",
+                "percentage": round(((previous_value - current_value) / previous_value) * 100, 2)
+            }
+
+        else:
+            return {
+                "trend": "Stable",
+                "direction": "None",
+                "percentage": 0
+            }
