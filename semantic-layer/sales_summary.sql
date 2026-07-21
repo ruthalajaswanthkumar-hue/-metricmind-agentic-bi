b708f219a7bb4fdce7b434491a9c2bdc43e5e423
sreeraj k <srjkk21@gmail.com>
2026-07-21 19:49:04 +0530

    Create sales_summary.sql

---
+++ semantic-layer/sales_summary.sql
@@ -0,0 +1,14 @@
+SELECT
+    COUNT(order_id) AS total_orders,
+
+    SUM(quantity) AS total_quantity_sold,
+
+    SUM(sales_amount) AS total_revenue,
+
+    AVG(sales_amount) AS average_order_value,
+
+    AVG(quantity) AS average_quantity_per_order,
+
+    SUM(discount) AS total_discount_given
+
+FROM {{ ref('stg_sales') }};
