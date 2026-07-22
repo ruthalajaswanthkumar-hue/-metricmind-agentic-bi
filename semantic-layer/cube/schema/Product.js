37ed26b0f70430749a923096fc9df3057c6a328b
sreeraj k <srjkk21@gmail.com>
2026-07-22 16:01:26 +0530

    Day 7-    ........+ updated product.js

--- semantic-layer/cube/schema/Product.js
+++ semantic-layer/cube/schema/Product.js
@@ -1,1 +1,38 @@
 
+cube(`Product`, {
+  sql: `SELECT * FROM stg_products`,
+
+  measures: {
+    totalProducts: {
+      sql: `product_id`,
+      type: `count`,
+      title: `Total Products`
+    },
+
+    averagePrice: {
+      sql: `unit_price`,
+      type: `avg`,
+      title: `Average Unit Price`
+    }
+  },
+
+  dimensions: {
+    productId: {
+      sql: `product_id`,
+      type: `string`,
+      title: `Product ID`
+    },
+
+    productName: {
+      sql: `product_name`,
+      type: `string`,
+      title: `Product Name`
+    },
+
+    category: {
+      sql: `category`,
+      type: `string`,
+      title: `Category`
+    }
+  }
+});
