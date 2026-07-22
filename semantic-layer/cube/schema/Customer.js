1df39a0ee4540461726970246c38d39352896beb
sreeraj k <srjkk21@gmail.com>
2026-07-22 15:55:25 +0530

    Update Customer.js

--- semantic-layer/cube/schema/Customer.js
+++ semantic-layer/cube/schema/Customer.js
@@ -1,1 +1,26 @@
 
+cube(`Customer`, {
+  sql: `SELECT * FROM customer_summary`,
+
+  measures: {
+    totalCustomers: {
+      sql: `total_customers`,
+      type: `sum`,
+      title: `Total Customers`
+    }
+  },
+
+  dimensions: {
+    region: {
+      sql: `region`,
+      type: `string`,
+      title: `Region`
+    },
+
+    city: {
+      sql: `city`,
+      type: `string`,
+      title: `City`
+    }
+  }
+});
