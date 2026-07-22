a1fb7859926eb221695f2b3bcc58089156e6229d
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-23 03:00:03 +0530

    Updated charts.py

--- backend/api/charts.py
+++ backend/api/charts.py
@@ -1,1 +1,12 @@
+from fastapi import APIRouter
 
+from backend.services.chart_service import ChartService
+from backend.schemas.chart_response import ChartResponse
+
+router = APIRouter()
+
+
+@router.get("/charts", response_model=ChartResponse)
+def get_charts():
+
+    return ChartService.get_all_charts()
