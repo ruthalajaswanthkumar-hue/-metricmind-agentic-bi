255fb33c07818189d17f351ac548558b744a4050
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-22 12:47:13 +0530

    Updated insight_test.py

--- analytics engine/insight_test.py
+++ analytics engine/insight_test.py
@@ -1,6 +1,29 @@
+"""
+Insight Generator Test
+"""
 
 from insight_generator import InsightGenerator
 
-print(InsightGenerator.generate("Revenue",120000,100000))
-print(InsightGenerator.generate("Profit",25000,30000))
-print(InsightGenerator.generate("Orders",500,500))
+print(
+    InsightGenerator.generate(
+        "Revenue",
+        600000,
+        500000
+    )
+)
+
+print(
+    InsightGenerator.region_insight("South")
+)
+
+print(
+    InsightGenerator.customer_insight(250)
+)
+
+print(
+    InsightGenerator.order_insight(420)
+)
+
+print(
+    InsightGenerator.recommendation()
+)
