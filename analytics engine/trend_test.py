f0d5562b4db88e1eedaeb37d3e7e3a9e9aa61af4
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-22 12:46:44 +0530

    Updated trend_test.py

--- analytics engine/trend_test.py
+++ analytics engine/trend_test.py
@@ -1,6 +1,10 @@
+ """
+Trend Analyzer Test
+"""
 
 from trend_analyzer import TrendAnalyzer
 
-result = TrendAnalyzer.analyze(120000, 100000)
+result = TrendAnalyzer.analyze(600000, 500000)
 
+print("Trend Analysis")
 print(result)
