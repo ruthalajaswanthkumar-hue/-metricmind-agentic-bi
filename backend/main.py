e8a66190377deb3b7e19ffe1d4d276f213da77a5
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-23 03:01:23 +0530

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-
+from backend.api.charts import router as charts_router
 from fastapi import FastAPI
 from pydantic import BaseModel
 from core.middleware import RequestMiddleware
@@ -9,7 +9,7 @@ from ai_agent.text_to_sql import generate_sql
 app = FastAPI()
 app.add_exception_handler(404, ExceptionHandler.not_found)
 app.add_middleware(RequestMiddleware)
-
+app.include_router(charts_router)
 
 class ChatRequest(BaseModel):
     question: str
