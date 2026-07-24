e75fa31b927cca6fb48b1c1bdd737598e4956eb2
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-25 03:15:36 +0530

    Updated dashboard_engine.py

--- analytics engine/dashboard_engine.py
+++ analytics engine/dashboard_engine.py
@@ -1,49 +1,47 @@
- """
-Dashboard Intelligence Engine
-"""
-from analytics_pipeline import AnalyticsPipeline
-dashboard_summary(data)
-pipeline = AnalyticsPipeline()
-return dashboard
-return {
-    "dashboard": dashboard,
-    "analytics": analytics
-}
+ from analytics_pipeline import AnalyticsPipeline
 
-analytics = pipeline.generate_dashboard(data)
 class DashboardEngine:
 
     @staticmethod
     def dashboard_summary(data):
-        """
-        Generates dashboard information from processed business data.
-        """
+
+        pipeline = AnalyticsPipeline()
+
+        analytics = pipeline.generate_dashboard(data)
 
         dashboard = {
-            "cards": [
+
+            "cards":[
                 {
-                    "title": "Revenue",
-                    "value": data.get("Revenue", 0)
+                    "title":"Revenue",
+                    "value":data.get("Revenue",0)
                 },
                 {
-                    "title": "Profit",
-                    "value": data.get("Profit", 0)
+                    "title":"Profit",
+                    "value":data.get("Profit",0)
                 },
                 {
-                    "title": "Orders",
-                    "value": data.get("Orders", 0)
+                    "title":"Orders",
+                    "value":data.get("Orders",0)
                 },
                 {
-                    "title": "Customers",
-                    "value": data.get("Customers", 0)
+                    "title":"Customers",
+                    "value":data.get("Customers",0)
                 }
             ],
 
-            "charts": [
+            "charts":[
                 "Bar Chart",
                 "Line Chart",
                 "Pie Chart"
             ]
+
         }
 
-        return dashboard
+        return {
+
+            "dashboard": dashboard,
+
+            "analytics": analytics
+
+        }
