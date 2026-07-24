3b701bcfa5c097eb6c56f68d2565d63a1de651ce
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-25 03:31:32 +0530

    Updated sales_summary.sql

--- semantic-layer/sales_summary.sql
+++ semantic-layer/sales_summary.sql
@@ -1,12 +1,12 @@
-SELECT
+ SELECT
     COUNT(order_id) AS total_orders,
 
     SUM(quantity) AS total_quantity_sold,
 
-    SUM(sales_amount) AS total_revenue, 
+    SUM(sales_amount) AS total_revenue,
 
     SUM(profit) AS total_profit,
-    
+
     AVG(sales_amount) AS average_order_value,
 
     AVG(quantity) AS average_quantity_per_order,
