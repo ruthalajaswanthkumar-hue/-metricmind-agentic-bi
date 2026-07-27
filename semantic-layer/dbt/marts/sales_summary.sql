3d982428dc2a8a4d35c5b7a30b71326de877700b
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-07-27 17:19:45 +0530

    Update sales_summary.sql

--- semantic-layer/dbt/marts/sales_summary.sql
+++ semantic-layer/dbt/marts/sales_summary.sql
@@ -1,1 +1,17 @@
 
+SELECT
+    COUNT(order_id) AS total_orders,
+
+    SUM(quantity) AS total_quantity_sold,
+
+    SUM(sales_amount) AS total_revenue,
+
+    SUM(profit) AS total_profit,
+
+    AVG(sales_amount) AS average_order_value,
+
+    AVG(quantity) AS average_quantity_per_order,
+
+    SUM(discount) AS total_discount_given
+
+FROM {{ ref('stg_sales') }};
