01795d7fda6e920e08eb3762ef54eab80c447582
neelamnithusha <neelamnithusha@gmail.com>
2026-07-27 23:18:31 +0530

    Updated KPICard.tsx

--- frontend_2/components/dashboard/KPICard.tsx
+++ frontend_2/components/dashboard/KPICard.tsx
@@ -1,66 +1,25 @@
+"use client";
+
 interface KPICardProps {
-
-title:string;
-value:string;
-icon:React.ReactNode;
-percentage:string;
-trend:string;
-
+  title: string;
+  value: string | number;
 }
 
-
 export default function KPICard({
+  title,
+  value,
+}: KPICardProps) {
+  return (
+    <div className="bg-white shadow-lg rounded-xl p-6 border">
 
-title,
-value,
-icon,
-percentage,
-trend
+      <h3 className="text-gray-500 text-lg">
+        {title}
+      </h3>
 
-}:KPICardProps){
+      <p className="text-3xl font-bold mt-2">
+        {value}
+      </p>
 
-
-return(
-
-<div className="bg-white p-6 rounded-xl shadow-md">
-
-
-<div className="flex justify-between items-center">
-
-
-<div>
-
-<h3 className="text-gray-500">
-{title}
-</h3>
-
-
-<h2 className="text-3xl font-bold text-black mt-2">
-{value}
-</h2>
-
-
-</div>
-
-
-<div className="text-4xl text-blue-600">
-{icon}
-</div>
-
-
-</div>
-
-
-
-<p className="mt-4 text-green-600">
-
-{percentage} {trend}
-
-</p>
-
-
-</div>
-
-)
-
+    </div>
+  );
 }
