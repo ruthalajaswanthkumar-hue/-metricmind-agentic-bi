454e8cd3526a97bf97df72d02b8746f3593c3603
neelamnithusha <neelamnithusha@gmail.com>
2026-07-27 23:18:59 +0530

    Updated DataTable.tsx

--- frontend_2/components/dashboard/DataTable.tsx
+++ frontend_2/components/dashboard/DataTable.tsx
@@ -1,1 +1,43 @@
+"use client";
 
+interface Product {
+  product: string;
+  revenue: number;
+  profit: number;
+  quantity: number;
+}
+
+interface DataTableProps {
+  data: Product[];
+}
+
+export default function DataTable({ data }: DataTableProps) {
+  return (
+    <div className="overflow-x-auto mt-6">
+      <table className="min-w-full border border-gray-300">
+        <thead className="bg-blue-600 text-white">
+          <tr>
+            <th className="p-3 border">Product</th>
+            <th className="p-3 border">Revenue</th>
+            <th className="p-3 border">Profit</th>
+            <th className="p-3 border">Quantity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((item, index) => (
+            <tr
+              key={index}
+              className="text-center hover:bg-gray-100"
+            >
+              <td className="border p-2">{item.product}</td>
+              <td className="border p-2">${item.revenue}</td>
+              <td className="border p-2">${item.profit}</td>
+              <td className="border p-2">{item.quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
