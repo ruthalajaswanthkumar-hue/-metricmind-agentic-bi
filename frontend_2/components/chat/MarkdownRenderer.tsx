ad855c4c3d31097f8792bd188337157da3118b17
neelamnithusha <neelamnithusha@gmail.com>
2026-07-27 23:14:55 +0530

    Updated MarkdownRenderer.tsx

--- frontend_2/components/chat/MarkdownRenderer.tsx
+++ frontend_2/components/chat/MarkdownRenderer.tsx
@@ -1,1 +1,33 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import CodeBlock from "./CodeBlock";
 
+interface MarkdownRendererProps {
+  content: string;
+}
+
+export default function MarkdownRenderer({
+  content,
+}: MarkdownRendererProps) {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className } = props;
+
+          const match = /language-(\w+)/.exec(className || "");
+
+          return (
+            <CodeBlock
+              language={match ? match[1] : "text"}
+              value={String(children).replace(/\n$/, "")}
+            />
+          );
+        },
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+}
