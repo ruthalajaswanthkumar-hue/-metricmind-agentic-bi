6fcfa3ba94cbb16eafd87358ec0a43e306296925
neelamnithusha <neelamnithusha@gmail.com>
2026-07-27 23:15:47 +0530

    Updated CodeBlock.tsx

--- frontend_2/components/chat/CodeBlock.tsx
+++ frontend_2/components/chat/CodeBlock.tsx
@@ -1,1 +1,30 @@
+"use client";
 
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+interface CodeBlockProps {
+  language: string;
+  value: string;
+}
+
+export default function CodeBlock({
+  language,
+  value,
+}: CodeBlockProps) {
+  return (
+    <div className="my-4 overflow-x-auto rounded-lg">
+      <SyntaxHighlighter
+        language={language}
+        style={oneDark as { [key: string]: React.CSSProperties }}
+        customStyle={{
+          margin: 0,
+          borderRadius: "0.5rem",
+          fontSize: "14px",
+        }}
+      >
+        {value}
+      </SyntaxHighlighter>
+    </div>
+  );
+}
