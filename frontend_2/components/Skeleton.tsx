d332f2e6aefe5a4c031b6fba30da94033290b3a4
neelamnithusha <neelamnithusha@gmail.com>
2026-07-28 23:25:54 +0530

    Updated Skeleton.tsx

--- frontend_2/components/Skeleton.tsx
+++ frontend_2/components/Skeleton.tsx
@@ -1,1 +1,56 @@
+"use client";
 
+
+import { motion } from "framer-motion";
+
+
+export default function Skeleton(){
+
+return(
+
+<motion.div
+
+className="
+space-y-4
+animate-pulse
+"
+
+initial={{
+opacity:0
+}}
+
+animate={{
+opacity:1
+}}
+
+>
+
+
+<div className="
+h-6
+bg-gray-300
+rounded
+w-3/4
+"/>
+
+
+<div className="
+h-32
+bg-gray-300
+rounded
+"/>
+
+
+<div className="
+h-6
+bg-gray-300
+rounded
+w-1/2
+"/>
+
+
+</motion.div>
+
+)
+
+}
