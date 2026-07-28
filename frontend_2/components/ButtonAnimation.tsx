9cfff69b80acb719aa424a1e8b1cf5876557da50
neelamnithusha <neelamnithusha@gmail.com>
2026-07-28 23:21:52 +0530

    Updated ButtonAnimation.tsx

--- frontend_2/components/ButtonAnimation.tsx
+++ frontend_2/components/ButtonAnimation.tsx
@@ -1,1 +1,48 @@
+"use client";
 
+
+import {motion} from "framer-motion";
+
+
+export default function ButtonAnimation({
+
+children
+
+}:{
+
+children:React.ReactNode
+
+}){
+
+
+return(
+
+<motion.button
+
+whileHover={{
+scale:1.05
+}}
+
+whileTap={{
+scale:0.95
+}}
+
+className="
+px-5
+py-2
+bg-blue-600
+text-white
+rounded-lg
+"
+
+>
+
+{children}
+
+
+</motion.button>
+
+
+)
+
+}
