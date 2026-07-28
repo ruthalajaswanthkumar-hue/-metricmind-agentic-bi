de14964ad10e40809ece7c65f4888009fb598748
neelamnithusha <neelamnithusha@gmail.com>
2026-07-28 23:24:22 +0530

    Updated Loading.tsx

--- frontend_2/components/Loading.tsx
+++ frontend_2/components/Loading.tsx
@@ -1,1 +1,25 @@
+"use client";
 
+import { motion } from "framer-motion";
+
+
+export default function Loading() {
+
+  return (
+    <div className="flex justify-center items-center h-40">
+
+      <motion.div
+        className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full"
+        animate={{
+          rotate:360,
+        }}
+        transition={{
+          duration:1,
+          repeat:Infinity,
+          ease:"linear",
+        }}
+      />
+
+    </div>
+  );
+}
