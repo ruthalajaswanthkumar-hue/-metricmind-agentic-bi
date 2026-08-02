8f493f923b1f2d5b341f8c1a7255b47a32250482
neelamnithusha <neelamnithusha@gmail.com>
2026-08-02 10:48:48 +0530

    Updated page.tsx

--- frontend_2/app/page.tsx
+++ frontend_2/app/page.tsx
@@ -1,15 +1,20 @@
 import Hero from "@/components/Hero";
-import CTA from "@/components/CTA";
 import Features from "@/components/Features";
 import About from "@/components/About";
+import CTA from "@/components/CTA";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <>
+    <main className="overflow-x-hidden bg-white dark:bg-slate-950">
+
       <Hero />
-      <CTA />
+
       <Features />
+
       <About />
-    </>
+
+      <CTA />
+
+    </main>
   );
 }
