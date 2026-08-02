cb6cf308d0c40b30f5ea36b5dce96f2ee14bca42
neelamnithusha <neelamnithusha@gmail.com>
2026-08-02 10:48:26 +0530

    Updated layout.tsx

--- frontend_2/app/layout.tsx
+++ frontend_2/app/layout.tsx
@@ -1,25 +1,37 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import ThemeProvider from "@/components/ThemeProvider";
 import Layout from "@/components/layout/Layout";
+
 
 export const metadata: Metadata = {
   title: "MetricMind",
-  description: "AI Powered Business Intelligence Dashboard",
+  description: "AI Powered Business Intelligence",
 };
+
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+
       <body>
-        <Layout>
-          {children}
-        </Layout>
+
+        <ThemeProvider>
+
+          <Layout>
+            {children}
+          </Layout>
+
+        </ThemeProvider>
+
       </body>
+
     </html>
   );
 }
