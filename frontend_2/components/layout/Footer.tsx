92a2298286762e437ca73e1e28264ffc6cba0e59
neelamnithusha <neelamnithusha@gmail.com>
2026-08-02 12:41:38 +0530

    Updated Footer.tsx

--- frontend_2/components/layout/Footer.tsx
+++ frontend_2/components/layout/Footer.tsx
@@ -1,12 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-4 px-6 flex justify-between items-center">
-      <p>© 2026 MetricMind. All rights reserved.</p>
-
-      <div className="flex gap-6">
-        <span>Version 1.0.0</span>
-        <span>Team: MetricMind</span>
-      </div>
-    </footer>
-  );
+  return null;
 }
