0c1356502c1a853d90a1b7878d297b66c98d42fe
neelamnithusha <neelamnithusha@gmail.com>
2026-08-02 10:43:06 +0530

    Updated page.tsx

--- frontend_2/app/chat/page.tsx
+++ frontend_2/app/chat/page.tsx
@@ -1,16 +1,9 @@
 import ChatBox from "@/components/chat/ChatBox";
 
-
 export default function ChatPage() {
-
   return (
-
-    <main className="min-h-screen bg-gray-100 p-5">
-
+    <main className="min-h-screen bg-gray-100 p-5 dark:bg-slate-950">
       <ChatBox />
-
     </main>
-
   );
-
 }
