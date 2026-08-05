d8543697952b8d70e5fc9fc85ad3017bdfa22719
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-08-05 05:54:46 +0530

    Add script to list tables in SQLite database

---
+++ check_db.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect("metricmind.db")
+
+cursor = conn.cursor()
+
+tables = cursor.execute(
+    "SELECT name FROM sqlite_master WHERE type='table';"
+).fetchall()
+
+print("Tables:")
+for table in tables:
+    print(table[0])
+
+conn.close()
