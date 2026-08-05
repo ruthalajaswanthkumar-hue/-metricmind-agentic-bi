3a89f36090dd58efa5cae8f11684dd9b8a7d8180
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-08-05 05:51:14 +0530

    updated ChatLayout component wrapping children in Layout

--- frontend_2/app/chat/layout.tsx
+++ frontend_2/app/chat/layout.tsx
@@ -1,1 +1,10 @@
 
+import Layout from "@/components/layout/Layout";
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Layout>{children}</Layout>;
+}
