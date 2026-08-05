f4c6bebabc2549b33640c846ef4668d601261a4d
ruthalajaswanthkumar-hue <ruthalajaswanthkumar@gmail.com>
2026-08-05 06:01:53 +0530

    updated  from CSV files
    Load multiple CSV files into SQLite database tables.

--- load_databse.py
+++ load_databse.py
@@ -1,1 +1,41 @@
 
+import pandas as pd
+import sqlite3
+
+
+DB_PATH = "backend/metricmind.db"
+
+
+conn = sqlite3.connect(DB_PATH)
+
+
+files = {
+    "Sales": "datsets/Sales.csv",
+    "Customer": "datsets/Customer.csv",
+    "Order": "datsets/Order.csv",
+    "Products": "datsets/Products.csv",
+    "Date": "datsets/Date.csv",
+    "Region": "datsets/Region.csv",
+    "Category": "datsets/Category.csv"
+}
+
+
+for table, file in files.items():
+
+    df = pd.read_csv(file)
+
+    df.to_sql(
+        table,
+        conn,
+        if_exists="replace",
+        index=False
+    )
+
+    print(
+        f"{table} loaded successfully"
+    )
+
+
+conn.close()
+
+print("Database loading completed")
